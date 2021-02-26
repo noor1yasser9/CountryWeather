9145c0313961a0751da33f02747c10c389412824
OMEN <mrh61037@gmail.com>
2021-02-26 18:03:45 +0200

    update Retrofit Module

--- app/src/main/java/com/nurbk/ps/countryweather/di/RetrofitModule.kt
+++ app/src/main/java/com/nurbk/ps/countryweather/di/RetrofitModule.kt
@@ -9,8 +9,10 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 
@@ -24,6 +26,13 @@ object RetrofitModule {
         Retrofit.Builder()
             .baseUrl(baseUrl)
             .addConverterFactory(GsonConverterFactory.create())
+            .apply {
+                val builder = OkHttpClient.Builder()
+                builder.connectTimeout(5, TimeUnit.MINUTES) // connect timeout
+                    .writeTimeout(5, TimeUnit.MINUTES) // write timeout
+                    .readTimeout(5, TimeUnit.MINUTES) // read timeout
+                this.client(builder.build())
+            }
             .build()
 
     @Provides
@@ -45,7 +54,6 @@ object RetrofitModule {
     @Singleton
     fun weatherInterface() =
         InstaceRetrofit(ConstanceString.BASE_URL_WEATHER).create(WeatherInterface::class.java)
-
 
 
 }
