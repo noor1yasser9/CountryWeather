7e9f3d2deadcaf3042b35c0074276b6d8201ae9e
OMEN <mrh61037@gmail.com>
2021-02-28 21:47:51 +0200

    update weather adapter

--- app/src/main/java/com/nurbk/ps/countryweather/adapters/WeatherAdapter.kt
+++ app/src/main/java/com/nurbk/ps/countryweather/adapters/WeatherAdapter.kt
@@ -9,65 +9,22 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.nurbk.ps.countryweather.R
 import com.nurbk.ps.countryweather.databinding.ItemWeatherDayBinding
+import com.nurbk.ps.countryweather.databinding.ItemWeatherHourBinding
+import com.nurbk.ps.countryweather.model.weather.daysweather.ListItem
 import com.nurbk.ps.countryweather.model.weather.fivedayweather.ItemHourly
 import com.nurbk.ps.countryweather.utils.AppUtil
+import com.nurbk.ps.countryweather.utils.ConstanceString
 import java.util.*
 
-class WeatherAdapter : RecyclerView.Adapter<WeatherAdapter.WeatherViewHolder>() {
+class WeatherAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-//    private lateinit var colorss: IntArray
-//    private lateinit var colorsAlpha: IntArray
-//
+    var type = 1
 
-
-    inner class WeatherViewHolder(val mBinding: ItemWeatherDayBinding) :
+    inner class WeatherViewHolder(val mBinding: ItemWeatherHourBinding) :
         RecyclerView.ViewHolder(mBinding.root) {
         fun bind(
             item: ItemHourly, position: Int
-//                 ,color:Int,colorsAlpha:Int
         ) {
-//            mBinding.cardView.setCardBackgroundColor(color)
-//            val colors = intArrayOf(
-//                Color.TRANSPARENT,
-//                R.color.material_blue,
-//                Color.TRANSPARENT
-//            )
-//            val calendar = Calendar.getInstance(TimeZone.getDefault())
-//            calendar.timeInMillis = item.dt * 1000L
-//            if (AppUtil.isRTL(mBinding.root.context)) {
-//                mBinding.dayNameTextView.text = ConstanceString.DAYS_OF_WEEK_PERSIAN[calendar[Calendar.DAY_OF_WEEK] - 1]
-//            } else {
-//                mBinding.dayNameTextView.text = ConstanceString.DAYS_OF_WEEK[calendar[Calendar.DAY_OF_WEEK] - 1]
-//            }
-//            if (item.main!!.tempMax < 0 && item.main!!.tempMax > -0.5) {
-//                item.main!!.tempMax = 0.0
-//            }
-//            if (item.main!!.tempMin < 0 && item.main!!.tempMin > -0.5) {
-//                item.main!!.tempMin = 0.0
-//            }
-//            if (item.main!!.temp < 0 && item.main!!.temp > -0.5) {
-//                item.main!!.temp = 0.0
-//            }
-//            mBinding.tempTextView.text = String.format(
-//                Locale.getDefault(),
-//                "%.0f°",
-//                item.main!!.temp
-//            )
-//            mBinding.minTempTextView.text = String.format(
-//                Locale.getDefault(),
-//                "%.0f°",
-//                item.main!!.tempMin
-//            )
-//            mBinding.maxTempTextView.text = String.format(
-//                Locale.getDefault(),
-//                "%.0f°",
-//                item.main!!.tempMax
-//            )
-
-//
-//            val shape = GradientDrawable(GradientDrawable.Orientation.RIGHT_LEFT, colors)
-//            shape.shape = GradientDrawable.OVAL
-//            mBinding.shadowView.background = shape
             if (position == 2) {
                 mBinding.view.isGone = true
             }
@@ -91,8 +48,61 @@ class WeatherAdapter : RecyclerView.Adapter<WeatherAdapter.WeatherViewHolder>() 
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WeatherViewHolder {
-        return WeatherViewHolder(
+    inner class DaysWeatherViewHolder(val mBinding: ItemWeatherDayBinding) :
+        RecyclerView.ViewHolder(mBinding.root) {
+        fun bind(
+            item: ListItem
+        ) {
+
+            val calendar = Calendar.getInstance(TimeZone.getDefault())
+            calendar.timeInMillis = item.dt * 1000L
+            if (AppUtil.isRTL(mBinding.root.context)) {
+                mBinding.dayNameTextView.text =
+                    ConstanceString.DAYS_OF_WEEK_PERSIAN[calendar[Calendar.DAY_OF_WEEK] - 1]
+            } else {
+                mBinding.dayNameTextView.text =
+                    ConstanceString.DAYS_OF_WEEK[calendar[Calendar.DAY_OF_WEEK] - 1]
+            }
+            if (item.temp!!.max < 0 && item.temp!!.max > -0.5) {
+                item.temp!!.max = 0.0
+            }
+            if (item.temp!!.min < 0 && item.temp!!.min > -0.5) {
+                item.temp!!.min = 0.0
+            }
+            if (item.deg < 0 && item.deg > -0.5) {
+                item.deg = 0
+            }
+//            mBinding.tempTextView.text = String.format(
+//                Locale.getDefault(),
+//                "%.0f°",
+//                item.deg
+//            )
+            mBinding.minTempTextView.text = String.format(
+                Locale.getDefault(),
+                "%.0f°",
+                item.temp!!.min
+            )
+            mBinding.maxTempTextView.text = String.format(
+                Locale.getDefault(),
+                "%.0f°",
+                item.temp!!.max
+            )
+
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        if (viewType == 1)
+            return WeatherViewHolder(
+                DataBindingUtil.inflate(
+                    LayoutInflater.from(parent.context),
+                    R.layout.item_weather_hour,
+                    parent,
+                    false
+                )
+            )
+        return DaysWeatherViewHolder(
             DataBindingUtil.inflate(
                 LayoutInflater.from(parent.context),
                 R.layout.item_weather_day,
@@ -102,14 +112,18 @@ class WeatherAdapter : RecyclerView.Adapter<WeatherAdapter.WeatherViewHolder>() 
         )
     }
 
-    override fun onBindViewHolder(holder: WeatherViewHolder, position: Int) {
-        holder.bind(
-            data[position], position
-//            ,colorss[position],colorsAlpha[position]
-        )
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is WeatherViewHolder)
+            holder.bind(
+                data[position], position
+            )
+        else if (holder is DaysWeatherViewHolder)
+            holder.bind(
+                dataDay[position]
+            )
     }
 
-    override fun getItemCount() = data.size
+    override fun getItemCount() = if (type == 1) data.size else dataDay.size
 
 
     private val diffCallback = object : DiffUtil.ItemCallback<ItemHourly>() {
@@ -128,5 +142,26 @@ class WeatherAdapter : RecyclerView.Adapter<WeatherAdapter.WeatherViewHolder>() 
     var data: List<ItemHourly>
         get() = differ.currentList
         set(value) = differ.submitList(value)
+    private val diffCallbackDay = object : DiffUtil.ItemCallback<ListItem>() {
+        override fun areItemsTheSame(oldItem: ListItem, newItem: ListItem): Boolean {
+            return oldItem.dt == newItem.dt
+        }
 
+        override fun areContentsTheSame(oldItem: ListItem, newItem: ListItem): Boolean {
+            return oldItem.hashCode() == newItem.hashCode()
+        }
+    }
+
+
+    val differDay = AsyncListDiffer(this, diffCallbackDay)
+
+    var dataDay: List<ListItem>
+        get() = differDay.currentList
+        set(value) = differDay.submitList(value)
+
+    override fun getItemViewType(position: Int): Int {
+        if (type == 1)
+            return 1
+        return 0
+    }
 }
